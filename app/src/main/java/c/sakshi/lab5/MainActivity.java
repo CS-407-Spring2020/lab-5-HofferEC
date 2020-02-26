6109f4c57d3165dc11d85103d0de03c7853ea427
nbhoff@wisc.edu <>
2020-02-26 15:36:21 -0600

    M1

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -2,7 +2,9 @@ package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,17 +13,29 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     EditText et_username;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = getApplicationContext().getSharedPreferences("login", Context.MODE_PRIVATE);
+        if (sp.contains("u")) login(sp.getString("u", null));
         et_username = findViewById(R.id.et_username);
     }
 
     public void onButtonClick(View view){
+        login(et_username.getText().toString());
+    }
+
+    public void login(String u) {
+        if (u == null) {
+            Log.e("LOGIN", "Error, trying to login with null username");
+            return;
+        }
         Intent intent = new Intent(this, S2a.class);
-        intent.putExtra("u", et_username.getText().toString());
+        sp.edit().putString("u",u).apply();
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/c/sakshi/lab5/S2a.java
+++ app/src/main/java/c/sakshi/lab5/S2a.java
@@ -3,7 +3,9 @@ package c.sakshi.lab5;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,13 +15,20 @@ import android.widget.TextView;
 
 public class S2a extends AppCompatActivity {
 
+    SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s2a);
+        sp = getApplicationContext().getSharedPreferences("login", Context.MODE_PRIVATE);
+        String username = sp.getString("u", null);
+        if (username == null) {
+            Log.e("LOGIN", "Trying to launch S2a with null username in SP.");
+            logout();
+        }
+
         TextView tv_username = findViewById(R.id.tv_username);
-        Intent intent = this.getIntent();
-        String username = intent.getStringExtra("u");
         tv_username.setText("Welcome " + username + "!");
     }
 
@@ -37,12 +46,21 @@ public class S2a extends AppCompatActivity {
                 addNote();
                 return true;
             case R.id.itm_logout:
-                finish();
+                logout();
                 return true;
             default:
                 Log.e("MENU", "Misclassified menu item:" + item.toString());
                 return false;
         }
+    }
+
+    public void logout() {
+        Intent intent = new Intent(this, MainActivity.class);
+        if (sp.contains("u")) {
+            sp.edit().remove("u").apply();
+        }
+        startActivity(intent);
+        finish();
     }
 
     public void addNote(){
